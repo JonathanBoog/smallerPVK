23078a4a5e8176a42d19feaa5917787a32e9b3a4
Jonathan Lavén <jonathan.laven123@gmail.com>
2025-04-22 21:36:18 +0200

    add som more

---
+++ README.md
@@ -0,0 +1,3 @@
+# smallerPVK
+
+Developed with Unreal Engine 5
---
+++ Source/llmcallcode.py
@@ -0,0 +1,45 @@
+from openai import OpenAI
+from pydantic import BaseModel
+from typing import List, Dict, Union
+import os
+
+api_key = os.getenv("API_KEY")
+api = OpenAI(api_key = "")  # Nu fungerar det!
+
+system_prompt = '''Translate necessary parameters for the functions and return in given format.,  
+the move to location function is called move with parameters x,y,z, DET SKA ALLTID FINNAS tre parameterar "
+to make the character jump, jump har inga parametrar.
+delay : en parameter (i sekunder)
+
+om spelaren ber om att få göra saker i rad. varje del ska finnas i actions listan
+x=1 innebär i princip en 1 cm 
+om jag säger att jag vill gå x antal steg i en viss riktning, då ska ju du såklart öka koordinat värdena
+om användaren inte anger något -> inga actions
+'''
+
+
+class Action(BaseModel):
+    name: str
+    parameters: List[Union[str, float, int, List[float]]]  # Lista med olika typer av parametrar
+
+class JsonFormat(BaseModel):
+    actions: List[Action]
+
+def get_ai_response(user_input: str, sys_inp:str) -> str:
+    """Tar in en användarinput och returnerar svaret från OpenAI."""
+
+    full_prompt = sys_inp  + system_prompt
+    completion = api.beta.chat.completions.parse(
+        model="gpt-4o-mini-2024-07-18",
+        messages=[
+            {"role": "system", "content": full_prompt},
+            {"role": "user", "content": user_input}
+        ],
+        response_format=JsonFormat,
+    )
+    return completion.choices[0].message.content
+
+if __name__ == "__main__":
+    user_input = input("Enter Prompt: ")
+    response = get_ai_response(user_input, "")
+    print("AI:", response)
